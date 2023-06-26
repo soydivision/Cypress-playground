56a573f1c1494e68f589a6f58a71da4b2e5ee1c4
Sadvakassov Bagzhan <sadvakasov.bagjan@gmail.com>
2023-06-26 14:42:23 +0600

    minor edit in log in test

--- log_in_test.cy.js
+++ log_in_test.cy.js
@@ -2,8 +2,8 @@ describe('template spec', () => {
   it('passes', () => {
     cy.visit('https://boardgamegeek.com/');
     cy.get('[ggloginbutton=""]').click();
-    cy.get('#inputUsername').type('valid_user');
-    cy.get('#inputPassword').type('valid_password').type('{enter}');
+    cy.get('#inputUsername').type('fake_user_name');
+    cy.get('#inputPassword').type('fake_password').type('{enter}');
     cy.get('[role="alert"]').should('be.visible');
   })
 })
